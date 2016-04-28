c9e2a9984235522cbc0c458ae2982cfc543df440
tdawg <trudowski@gmail.com>
2016-04-28 14:52:33 -0400

    fixed some left/right animation shenanigans

--- public/3rdParty/main.js
+++ public/3rdParty/main.js
@@ -1,6 +1,4 @@
-var white_spy, green_spy;
-
-//var wspy = new Spy();
+var white_spy;
 
 /**
  * New node file
@@ -25,49 +23,69 @@ var mainState = {
 
 	create : function() {
 		
-		//var btn = game.add.sprite(50, 50, 'button_right');
-		//btn.scale.setTo(0.25, 0.25);
-
 		lbutton = game.add.button(game.world.centerX - 95, 300, 'button_left', move_left, this, 2, 1, 0);
 		lbutton.scale.setTo(0.25, 0.25);
+		lbutton.onInputDown.add(buttonDownLeft, this);
+		lbutton.onInputUp.add(buttonUpLeft, this);
 
 		rbutton = game.add.button(game.world.centerX + 25, 300, 'button_right', move_right, this, 2, 1, 0);
 		rbutton.scale.setTo(0.25, 0.25);
+		rbutton.onInputDown.add(buttonDownRight, this);
+		rbutton.onInputUp.add(buttonUpRight, this);
 		
 		// This function is called after the preload function
 		// Here we set up the game, display sprites, etc.
 		white_spy = new Spy(game, 0, 0, white_spy_def);
 		//white_spy = new Spy(game.add.sprite(0, 0, 'spies', 'wspy_stand'));
-		green_spy = game.add.sprite(0, 50, 'spies', 'gspy_stand');
+		//green_spy = game.add.sprite(0, 50, 'spies', 'gspy_stand');
 		
 		//white_spy.animations.add('wspy_rrun', ['wspy_rstand', 'wspy_rrun'], 6, true, false);
-		green_spy.animations.add('gspy_rrun', ['gspy_rstand', 'gspy_rrun'], 6, true, false);
+		//green_spy.animations.add('gspy_rrun', ['gspy_rstand', 'gspy_rrun'], 6, true, false);
 		
 		//white_spy.animations.play('run');
-		green_spy.animations.play('gspy_rrun');
+		//green_spy.animations.play('gspy_rrun');
 		
-		white_spy.setAction('run_right');
+		//white_spy.setAction('run_right');
 	},
 
 	update : function() {
 		// This function is called 60 times per second
 		// It contains the game's logic
+		/*
 		green_spy.x += 2;
 		if (green_spy.x > 800) {
 			green_spy.x = -50;
 		}
+		*/
 		
 		white_spy.updateMovement();
 		
 	},
 };
 
+function buttonDownLeft() {
+	white_spy.moveLeft();	
+}
+
+function buttonUpLeft() {
+	white_spy.stopMoving();	
+}
+
+function buttonDownRight() {
+	white_spy.moveRight();	
+}
+
+function buttonUpRight() {
+	white_spy.stopMoving();	
+}
+
 function move_left () {
-	console.log('left');
+	//console.log('left');
 }
 
 function move_right () {
-	console.log('right');
+	//console.log('right');
+	//white_spy.setAction('run_right');
 }
 
 // Initialize Phaser, and create a 400px by 490px game
--- public/3rdParty/spy.js
+++ public/3rdParty/spy.js
@@ -21,14 +21,15 @@ var green_spy_def = {
 
 var Spy = (function() 
 {
+	var speed, action;
 	
 	var Spy = function(game, x, y, spy_def) 
 	{
 		Phaser.Particle.call(this, game, x, y, 'spies', spy_def.stand);
 
 		// Associate specific image frames with animation sequences
-		this.animations.add('run_right', [spy_def.stand_right, spy_def.run_right], 6, true, false);
-		this.animations.add('run_left', [spy_def.stand_left, spy_def.run_left], 6, true, false);
+		this.animations.add('run_right', [spy_def.stand_right, spy_def.run_right], 4, true, false);
+		this.animations.add('run_left', [spy_def.stand_left, spy_def.run_left], 4, true, false);
 
 		game.add.existing(this);		
 		console.log('spy()');
@@ -38,7 +39,6 @@ var Spy = (function()
 	Spy.prototype = Object.create(Phaser.Particle.prototype);
 	Spy.prototype.constructor = Spy;
 	
-	var action;
 	Spy.prototype.setAction = function(new_action)
 	{
 		// stop previous action
@@ -49,23 +49,64 @@ var Spy = (function()
 		console.log('playing animation[' + new_action + ']');
 	};
 	
+	Spy.prototype.checkAction = function(action)
+	{
+		// check action
+		// if not action is not correct, call setAction
+		if ( this.action != action )
+			this.setAction(action);
+				
+		// check animation
+		var anim = this.animations.getAnimation(action);
+		
+		// if an animation is registered and it's not playing, call setAction
+		if ( anim != null && !anim.isPlaying )
+			this.setAction(action);
+	}
+	
 	Spy.prototype.updateMovement = function()
 	{
 		switch ( this.action )
 		{
+		case 'run_left':
+			this.x -= this.speed;
+			if (this.x <= 0) 
+				this.x = 0;
+			break;
 		case 'run_right':
-			this.x += 5;
+			this.x += this.speed;
 			if (this.x > 800) 
 				this.x = -50;
 			break;
 		default:
-			console.error("We have no logic for action[" + this.action + "]");
+			//console.error("We have no logic for action[" + this.action + "]");
 			break;
 		}
 
 	};
-	
 
+	Spy.prototype.moveRight = function()
+	{
+		this.checkAction('run_right');
+		
+		this.speed = 4;
+	}
+	
+	Spy.prototype.moveLeft = function()
+	{
+		this.checkAction('run_left');
+		
+		this.speed = 4;
+	}
+
+	Spy.prototype.stopMoving = function()
+	{
+		this.speed = 0;
+		
+		this.animations.stop(null, true);
+	}
+
+	
 	/*
 	CoinParticle.prototype.onEmit = function() {
 		this.animations.stop("rotate", true);
