c70c814557e76d7f9bd7a0e253ba0dfe73e68513
tim <trudowski@gmail.com>
2020-03-12 23:53:32 -0400

    - added joystick plugin to main scene

--- public/js/phaser/plugins/game_loop.js
+++ public/js/phaser/plugins/game_loop.js
@@ -13,20 +13,42 @@ var GameLoop = new Phaser.Class({
 	{
 		this.load.image("room","img/demoroom.png");
 
-		//this.load.image('mech', 'assets/pics/titan-mech.png');
+		const url = './js/phaser/plugins/joystick.js';
+		this.load.plugin('rexvirtualjoystickplugin', url, true);
 	},
 
 	create: function ()
 	{
-		this.add.sprite(0, 0, "room");
+		this.add.sprite(0, 0, "room").setOrigin(0, 0);
 
-		this.input.once('pointerdown', function (event)
-		{
-			console.log('From PlayerSelection to GameLoop');
+		this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
+			x: 200,
+			y: this.cameras.main.displayHeight - 125,
+			radius: 100,
+			base: this.add.circle(0, 0, 100, 0x888888),
+			thumb: this.add.circle(0, 0, 50, 0xcccccc),
+			// dir: '8dir',   // 'up&down'|0|'left&right'|1|'4dir'|2|'8dir'|3
+			// forceMin: 16,
+			// enable: true
+		}).on('update', this.dumpJoyStickState, this);
 
-			this.scene.start('player_selection');
+		this.text = this.add.text(0, 0);
+		this.dumpJoyStickState.call( this );
+	},
 
-		}, this);
+	dumpJoyStickState: function ()
+	{
+		let cursorKeys = this.joyStick.createCursorKeys();
+		let s = 'Key down: ';
+		for (let name in cursorKeys) {
+			if (cursorKeys[name].isDown) {
+				s += name + ' ';
+			}
+		}
+		s += '\n';
+		s += ('Force: ' + Math.floor(this.joyStick.force * 100) / 100 + '\n');
+		s += ('Angle: ' + Math.floor(this.joyStick.angle * 100) / 100 + '\n');
+		this.text.setText(s);
 	}
 
 });
