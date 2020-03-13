431bce96e063c1c2034057c41512ce5e71648772
tim <trudowski@gmail.com>
2020-03-12 22:34:12 -0400

    - added logic to replace color in an image.  this is currently being using on the player selection screen.

--- public/js/phaser/plugins/player_selection.js
+++ public/js/phaser/plugins/player_selection.js
@@ -16,6 +16,7 @@ var PlayerSelection = new Phaser.Class({
 			this.text2;
 			this.wheel;
 			this.wheelpos;
+			this.mycolor;
 		},
 
 	preload: function ()
@@ -31,9 +32,6 @@ var PlayerSelection = new Phaser.Class({
 
 		this.input.mouse.disableContextMenu();
 
-		const spy = this.add.image( this.cameras.main.centerX, this.cameras.main.centerY, "spyWhite" );
-		spy.setScale( 3 );
-
 		this.wheel = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'wheel');
 		const wheelScale = 0.75;
 		this.wheel.setScale( wheelScale );
@@ -47,25 +45,114 @@ var PlayerSelection = new Phaser.Class({
 
 		let graphics = this.add.graphics( { x: 0, y: 0 });
 
+		let texture = this.copyTextureFromImage( 'spyWhite', 'spyDynamic' );
+
+		const spy = this.add.image( this.cameras.main.centerX, this.cameras.main.centerY, "spyDynamic" );
+		spy.setScale( 3 );
+
 		let _this = this;
 
 		this.input.on('pointermove', function (pointer) {
 
-			let color = _this.textures.getPixel((pointer.x - _this.wheelpos.x1) / wheelScale, (pointer.y - _this.wheelpos.y1) / wheelScale, 'wheel');
+			_this.mycolor = _this.textures.getPixel((pointer.x - _this.wheelpos.x1) / wheelScale, (pointer.y - _this.wheelpos.y1) / wheelScale, 'wheel');
 
 			graphics.clear();
 
-			if (color)
+			if (_this.mycolor)
 			{
 				graphics.lineStyle(1, 0x000000, 1);
 				graphics.strokeRect(pointer.x - 1, pointer.y - 1, 34, 34);
 
-				graphics.fillStyle(color.color, 1);
+				graphics.fillStyle(_this.mycolor.color, 1);
 				graphics.fillRect(pointer.x, pointer.y, 32, 32);
 			}
 		});
 
+		this.input.on('pointerup', function(pointer)
+		{
+			if (_this.mycolor)
+			{
+				// replace all white pixels on our spy with the color chosen by the user
+				_this.updateTextureColor( texture, 0xFFFFFF, _this.mycolor );
+			}
+		});
+
+
+
 		// todo: also add in pointerout to reset tooltip
+	},
+
+	/**
+	 * Make a copy of the texture identified by srcTextureKey and store it on a new canvas identified by destTextureKey
+	 * @param srcTextureKey
+	 * @param destTextureKey
+	 * @returns {*}
+	 */
+	copyTextureFromImage : function( srcTextureKey, destTextureKey )
+	{
+		let spySrc = this.textures.get(srcTextureKey).getSourceImage();
+
+		return this.textures.createCanvas(destTextureKey, spySrc.width, spySrc.height).draw(0, 0, spySrc);
+	},
+
+	/**
+	 * Replace all occurances of the color 'replaceColor' within 'texture' with the color 'newColor'
+	 * @param texture the texture containing white pixels
+	 * @param replaceColor the color being replaced
+	 * @param newColor the new color being added
+	 */
+	updateTextureColor : function( texture, replaceColor, newColor )
+	{
+		let pixels = texture.getPixels( 0, 0, texture.width, texture.height );
+
+		let pixel;
+		for ( let row = 0; row < pixels.length; row++ )
+		{
+			for ( let col = 0; col < pixels[row].length; col++ )
+			{
+				pixel = pixels[row][col];
+
+				// look for white
+				if ( pixel.color === replaceColor )
+				{
+					texture.setPixel( pixel.x, pixel.y, newColor.r, newColor.g, newColor.b );
+				}
+			}
+		}
+
+		texture.refresh();
+	},
+
+	replaceColor : function( img )
+	{
+		let spySrc = this.textures.get('spyWhite').getSourceImage();
+
+		let texture = this.textures.createCanvas('tmpspy', spySrc.width, spySrc.height).draw(0, 0, spySrc);
+
+		console.log('tex> ', texture);
+		let pixels = texture.getPixels( 0, 0, texture.width, texture.height );
+
+		const white = 0xFFFFFF;
+		const color = 0xFF0000;
+
+		let pixel;
+		for ( let row = 0; row < pixels.length; row++ )
+		{
+			for ( let col = 0; col < pixels[row].length; col++ )
+			{
+				pixel = pixels[row][col];
+
+				if ( pixel.color === white )
+				{
+					texture.setPixel( pixel.x, pixel.y, 255, 0, 0 );
+				}
+			}
+		}
+
+		texture.refresh();
+
+		let tmp = this.add.image( 50, 200, 'tmpspy' );
+		tmp.setScale( 3 );
 	},
 
 	update : function()
