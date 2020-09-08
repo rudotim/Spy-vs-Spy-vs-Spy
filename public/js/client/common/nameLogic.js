8aada038bdc1753a781b970e3a67ed0e244d1314
tim <trudowski@gmail.com>
2020-09-08 13:40:35 -0400

    - removed unnecessary interval logic remaining from the site which I stole the sidenav code.

--- public/js/client/common/nameLogic.js
+++ public/js/client/common/nameLogic.js
@@ -81,18 +81,4 @@ function toggleSidenav() {
 	document.body.classList.toggle('noscroll');
 }
 
-// ==============
-// Automated Demo
-
-const checkbox = document.querySelector('input');
-const interval = setInterval(toggleSidenav, 1500);
-
-document.addEventListener('mousemove', removeInterval);
-document.addEventListener('click', removeInterval);
-
-function removeInterval() {
-	clearInterval(interval);
-	document.removeEventListener('mousemove', removeInterval);
-	document.removeEventListener('click', removeInterval);
-}
 
