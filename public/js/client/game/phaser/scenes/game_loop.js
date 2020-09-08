a5da9a864fc3655a6907633579cc883b73155158
tim <trudowski@gmail.com>
2020-09-08 12:57:21 -0400

    - game start now shows a white spy (regardless of color chosen) in center of screen that can run left and right with the proper animations.

--- public/js/client/game/phaser/scenes/game_loop.js
+++ public/js/client/game/phaser/scenes/game_loop.js
@@ -1,4 +1,8 @@
-var GameLoop = new Phaser.Class({
+
+
+const NO_MOVEMENT=0, RUN_RIGHT=1, RUN_DOWN=2, RUN_LEFT=3, RUN_UP=4;
+
+let GameLoop = new Phaser.Class({
 
 	Extends: Phaser.Scene,
 
@@ -6,11 +10,19 @@ var GameLoop = new Phaser.Class({
 
 		function GameLoop ()
 		{
+
 			Phaser.Scene.call(this, { key: 'game_loop' });
+
+			this.mySpy = undefined;
+			this.moving = NO_MOVEMENT;
 		},
 
 	preload: function ()
 	{
+		// load all spy sprite data
+		this.load.atlas('spies', 'img/spritesheet.png', 'img/sprites.json');
+
+
 		this.load.image("room","img/demoroom.png");
 
 		const url = '/js/client/game/phaser/plugins/joystick.js';
@@ -19,7 +31,60 @@ var GameLoop = new Phaser.Class({
 
 	create: function ()
 	{
+		// define our frames for the running aniimations
+		const data = {
+			"anims": [
+				{
+					"key": "run_right",
+					"type": "frame",
+					"frameRate" : 12,
+					"repeat" : -1,
+					"frames": [
+						{
+							"key": "spies",
+							"frame": "wspy_rstand",
+							"duration": 0,
+							"visible": false
+						},
+						{
+							"key": "spies",
+							"frame": "wspy_rrun",
+							"duration": 0,
+							"visible": false
+						},
+					]
+				},
+				{
+					"key": "run_left",
+					"type": "frame",
+					"frameRate" : 12,
+					"repeat" : -1,
+					"frames": [
+						{
+							"key": "spies",
+							"frame": "wspy_lstand",
+							"duration": 0,
+							"visible": false
+						},
+						{
+							"key": "spies",
+							"frame": "wspy_lrun",
+							"duration": 0,
+							"visible": false
+						},
+					]
+				},
+			]
+		};
+
+		this.anims.fromJSON(data);
 		this.add.sprite(0, 0, "room").setOrigin(0, 0);
+
+		this.add.image(150, 100, 'spies', 'wspy_stand');
+
+
+		this.mySpy = this.add.sprite(350, 230, 'spies');
+
 
 		this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
 			x: 200,
@@ -36,13 +101,65 @@ var GameLoop = new Phaser.Class({
 		this.dumpJoyStickState.call( this );
 	},
 
+
+	update: function()
+	{
+		if (this.moving === RUN_RIGHT)
+		{
+			this.mySpy.x += 2;
+		}
+		else if (this.moving === RUN_LEFT )
+		{
+			this.mySpy.x -= 2;
+		}
+
+	},
+
 	dumpJoyStickState: function ()
 	{
 		let cursorKeys = this.joyStick.createCursorKeys();
 		let s = 'Key down: ';
-		for (let name in cursorKeys) {
-			if (cursorKeys[name].isDown) {
+		for (let name in cursorKeys)
+		{
+			if (cursorKeys[name].isDown)
+			{
 				s += name + ' ';
+
+				if ( name === "right" )
+				{
+					if ( this.moving !== RUN_RIGHT )
+						this.mySpy.play( "run_right" );
+
+					this.moving = RUN_RIGHT;
+				}
+				else if ( name === "left" )
+				{
+					if ( this.moving !== RUN_LEFT )
+						this.mySpy.play( "run_left" );
+
+					this.moving = RUN_LEFT;
+				}
+			}
+			else
+			{
+				if ( name === "right")
+				{
+					if (this.moving === RUN_RIGHT)
+					{
+						this.mySpy.setFrame( "wspy_rstand" );
+						this.mySpy.anims.stop(null, true);
+						this.moving = NO_MOVEMENT;
+					}
+				}
+				else if ( name === "left")
+				{
+					if (this.moving === RUN_LEFT)
+					{
+						this.moving = NO_MOVEMENT;
+						this.mySpy.anims.stop(null, true);
+						this.mySpy.setFrame( "wspy_lstand" );
+					}
+				}
 			}
 		}
 		s += '\n';
